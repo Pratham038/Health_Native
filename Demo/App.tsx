47ecf24a01db0f2dd123bb07d9c21f0e724e1ab9
Pratham Haram <pratham.haram@salt-tech.com>
2023-07-13 10:13:24 +0530

    5 fetched

--- Demo/App.tsx
+++ Demo/App.tsx
@@ -25,6 +25,7 @@ import {
   LearnMoreLinks,
   ReloadInstructions,
 } from 'react-native/Libraries/NewAppScreen';
+import googleFit from 'react-native-google-fit';
 
 function App(): JSX.Element {
   const isDarkMode = useColorScheme() === 'dark';
@@ -35,6 +36,13 @@ function App(): JSX.Element {
     bloodGlu: 0,
     f: 0,
     c: 0,
+    moveMinutes: 0,
+    bloodPressure: 0,
+    Temp: 0,
+    heartRate: 0,
+    nutrition: 0,
+    oxygen: 0,
+    dailySteps: 0,
   });
 
   const opt = {
@@ -52,6 +60,17 @@ function App(): JSX.Element {
       const fSamples = await GoogleFit.getDailyDistanceSamples(opt);
       const cSamples = await GoogleFit.getDailyCalorieSamples(opt);
       const moveMinutes = await GoogleFit.getMoveMinutes(opt);
+      const bloodPressure = await GoogleFit.getBloodPressureSamples(opt);
+      const bodyTemp = await GoogleFit.getBodyTemperatureSamples(opt);
+      const hertRate = await GoogleFit.getHeartRateSamples(opt);
+      const nutrition = await GoogleFit.getDailyNutritionSamples(opt);
+      const oxygen = await GoogleFit.getOxygenSaturationSamples(opt);
+      const dailySteps = await GoogleFit.getDailyStepCountSamples(opt);
+      let x = dailySteps[1];
+      let x2 = x.steps[0].value;
+
+      // const weeklySteps = await GoogleFit.getWeeklySteps(,1);
+      // const steps = await GoogleFit.getDailySteps();
 
       const weight = wSamples[0]?.value;
       const height = hSamples[0]?.value;
@@ -59,13 +78,34 @@ function App(): JSX.Element {
       const distance = fSamples[0]?.distance || 0; // Updated property name
       const calories = cSamples[0]?.calorie || 0; // Updated property name
 
+      console.log('WEIGHT :', wSamples[0].value);
+      console.log('HEIGHT :', hSamples[0].value);
+      console.log('Blood Glucose :', bloodGluSamples);
+      console.log('Daily Distance :', fSamples);
+      console.log('Daily Calories : ', calories);
+      console.log('Move Minutes :', moveMinutes[0].duration);
+      console.log('Blood Pressure :', bloodPressure);
+      console.log('Temperature:', bodyTemp);
+      console.log('Heart Rate:', hertRate);
+      console.log('Nutrition:', nutrition);
+      console.log('Oxygen:', oxygen);
+      console.log('Daily Steps :', x2);
+      // console.log('Weekly Steps :', await GoogleFit.getWeeklySteps());
+      // console.log('Steps :', stepValue);
+
       setBody({
         weight: weight || 0,
         height: height || 0,
         bloodGlu: bloodGlucose || 0,
         f: distance || 0,
         c: calories || 0,
-        moveMinutes: moveMinutes || 0,
+        moveMinutes: moveMinutes[0].duration || 0,
+        bloodPressure: bloodPressure || 0,
+        Temp: bodyTemp || 0,
+        heartRate: hertRate || 0,
+        nutrition: nutrition || 0,
+        oxygen: oxygen || 0,
+        dailySteps: x2 || 0,
       });
     } catch (error) {
       console.log('Error fetching health data:', error);
@@ -111,13 +151,32 @@ function App(): JSX.Element {
         contentInsetAdjustmentBehavior="automatic"
         style={styles.scrollView}>
         <View style={styles.body}>
-          <Text style={styles.sectionTitle}>Height: {body.height}</Text>
-          <Text style={styles.sectionTitle}>Weight: {body.weight}</Text>
+          <Text style={styles.sectionTitle}>Weight: {String(body.weight)}</Text>
+          <Text style={styles.sectionTitle}>Height: {String(body.height)}</Text>
           <Text style={styles.sectionTitle}>
-            Blood Glucose: {body.bloodGlu}
+            Blood Glucose: {String(body.bloodGlu)}
           </Text>
-          <Text style={styles.sectionTitle}>Distance: {body.f}</Text>
-          <Text style={styles.sectionTitle}>Calories: {body.c}</Text>
+          <Text style={styles.sectionTitle}>Distance: {String(body.f)}</Text>
+          <Text style={styles.sectionTitle}>Calories: {String(body.c)}</Text>
+          <Text style={styles.sectionTitle}>
+            Move Minutes: {String(body.moveMinutes)}
+          </Text>
+          <Text style={styles.sectionTitle}>
+            Blood Pressure: {String(body.bloodPressure)}
+          </Text>
+          <Text style={styles.sectionTitle}>
+            Body Temperature: {String(body.Temp)}
+          </Text>
+          <Text style={styles.sectionTitle}>
+            Heart Rate: {String(body.heartRate)}
+          </Text>
+          <Text style={styles.sectionTitle}>
+            Nutrition: {String(body.nutrition)}
+          </Text>
+          <Text style={styles.sectionTitle}>Oxygen: {String(body.oxygen)}</Text>
+          <Text style={styles.sectionTitle}>
+            Daily Steps: {String(body.dailySteps)}
+          </Text>
         </View>
       </ScrollView>
     </SafeAreaView>
@@ -127,17 +186,17 @@ function App(): JSX.Element {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    backgroundColor: '#fff', // Set background color
   },
   scrollView: {
     flex: 1,
   },
-  body: {
-    padding: 16,
-  },
+  body: {padding: 50},
   sectionTitle: {
     fontSize: 18,
     fontWeight: 'bold',
     marginTop: 16,
+    color: '#00cc30', // Set text color
   },
 });
 
